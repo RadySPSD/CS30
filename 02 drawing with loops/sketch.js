8bc2eec8e1d55a1a12a62703f21f09a1d2d6eb4b
Radyspsd <6500688@spsd.sk.ca>
2024-02-27 15:25:31 -0600

    02 loop drawings
    updated corrected version

--- 02 drawing with loops/sketch.js
+++ 02 drawing with loops/sketch.js
@@ -3,13 +3,38 @@
 // February 27, 2024
 //using loops + Arrays to create some visuiliztions
 
+// global variables
+let xPos, yPos;
+
 function setup() {
   createCanvas(400, 300);
+  xPos = [width*0.05, width*0.95,width*0.05,width*0.95];
+  yPos = [height*0.05, height*0.05,height*0.95,height*0.95];
 }
 
 function draw() {
   background(220);
-  cornersAndMouse();
+  cornersAndMouseLoop();
+}
+
+function mousePressed(){
+  // this calls automaticlaly on a mousepress
+  xPos.push(mouseX);
+  yPos.push(mouseY);
+}
+
+function cornersAndMouseLoop(){
+  // a hopefully slightly more elegant version
+  let i = 0;
+  while (i < xPos.length){
+    let x = xPos[i];
+    let y = yPos[i];
+    circle(x,y,20);
+    i++;
+    line(x,y,mouseX,mouseY);
+
+  }
+  circle(mouseX,mouseY,20);
 }
 
 function cornersAndMouse(){

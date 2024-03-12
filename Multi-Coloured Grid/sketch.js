a6107386662da3d62bf4e3493d3f034fe54ffd19
Radyspsd <6500688@spsd.sk.ca>
2024-03-12 15:23:49 -0600

    done
    just comments are left

--- Multi-Coloured Grid/sketch.js
+++ Multi-Coloured Grid/sketch.js
@@ -1,36 +1,43 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Multi coloured Grid
+// Ahmed Rady
+// 3/12/2024
+// The squares in the grid get bigger if you left click, and smaller if you left click. if you press any keybourd key, the colour will change.
 
-let squareSize = 200;
+//variables
+let squareSize;
 let squareColour;
-let r; let g; let b;
+let r; let g; let b; //the colours
+let colorTime = 10; //for the noise functions
+let canvasSize = 200; //size of canvas (can be changed)
 
+squareSize = canvasSize/2; //adjusts grid to fit no matter the canvas size
 
-function setup() {
-  createCanvas(400, 400);
-  document.addEventListener("contextmenu", event => event.preventDefault());
+function setup() { //main function
+  createCanvas(canvasSize, canvasSize);
+  document.addEventListener("contextmenu", event => event.preventDefault());//right click menue turns off.
   background(220);
   gridLines();
 }
-
-
 
 function draw() {
 
 }
 
-function gridLines() {
-  for (let x = 0; x < width; x = x + squareSize) {
-    for (let y = 0; y < height; y = y + squareSize) {
+function gridLines() { // making the grids
+  for (let x = 0; x < width; x = x + squareSize) { //the squares width
+    for (let y = 0; y < height; y = y + squareSize) { //the squares height
+      
+      colorTime += .01; // much it changes by
 
-      r = random (0,255);
-      g = random (0,255);
-      b = random (0,255);
-    
+      r = noise(colorTime);
+      r = map(r,0,1,1,255);
+
+      g = noise(colorTime);
+      g = map(g,0,1,255,1);
+
+      b = noise(colorTime);
+      b = map(b,0,1,255,100);
+
       squareColour = color(r, g, b);
       fill(squareColour);
       square(x, y, squareSize);
@@ -44,7 +51,11 @@ function mousePressed(){
   }
 
   if ( mouseIsPressed && mouseButton === RIGHT){
-    if(squareSize > 10) {squareSize = squareSize / 2;}
+    if(squareSize > 20) {squareSize = squareSize / 2;}
   }
+  gridLines();
+}
+
+function keyPressed(){
   gridLines();
 }

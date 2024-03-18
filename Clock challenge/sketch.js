e2402428a4dfc1698e6c0e627be25e2dea01fb67
RadySPSD <6500688@spsd.sk.ca>
2024-03-18 15:20:10 -0600

    clock challange
    drew a circle

--- Clock challenge/sketch.js
+++ Clock challenge/sketch.js
@@ -7,9 +7,19 @@
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+  
+  circle(width/2,width/2,400);
+  let angle = 0;
+  while(angle < 45){
+    push();
+    rotate(angle);
+    line(800,50,0,0);
+    pop();
+    angle += 1;
+  }
 }

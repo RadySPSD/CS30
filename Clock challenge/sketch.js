fdca138ed3a985d7f5e22aff54a54a61bf926ebe
RadySPSD <6500688@spsd.sk.ca>
2024-03-19 14:48:22 -0600

    clock
    finished seconds hand

--- Clock challenge/sketch.js
+++ Clock challenge/sketch.js
@@ -8,18 +8,43 @@
 
 function setup() {
   createCanvas(800, 800);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(220);
-  
-  circle(width/2,width/2,400);
-  let angle = 0;
-  while(angle < 45){
-    push();
+  drawStaticClock();
+  drawClockHands();
+}
+
+function drawStaticClock(){
+  translate(width/2,width/2); // do this to get the lines in the middle
+  push();
+  circle(0,0,300);
+  let count = 0;let angle = 6;
+  while (count < 60){
+    if (count % 5 === 0){
+      strokeWeight(3);
+      line(110,0,140,0);
+    }
+
+    
+
+    else{
+      strokeWeight(1);
+      line(125,0,140,0);
+    }
+    
     rotate(angle);
-    line(800,50,0,0);
-    pop();
-    angle += 1;
+    count ++;
   }
+}
+
+function drawClockHands(){
+  push();
+  stroke(2000,0,0);
+  strokeWeight(1);
+  rotate(second()*6);
+  line(0,0,0,130);
+  pop();
 }

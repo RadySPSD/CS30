e56959ef91b1592bd77fccf451c9a8d0502e7573
RadySPSD <6500688@spsd.sk.ca>
2024-03-27 15:09:08 -0600

    done now
    now done

--- Terrain Generation/sketch.js
+++ Terrain Generation/sketch.js
@@ -1,7 +1,7 @@
 // Terrain Generation
 // Ahmed Rady
 // 25/3/2024
-// terain that generates and a flag that goes to the highest point and a line that stays on the avrage height
+// terrain that generates and a flag that goes to the highest point and a line that stays on the avrage height
 
 //variables
 let rectWidth = 1;
@@ -19,21 +19,23 @@ function setup() {
 }
 
 //terrain generator
-function drawRectangles(){
-  let highestY = height; 
+function drawRectangles(){ 
   let rectHeight;
   fill(0);
   let timeX = rectTime;
   
-  //reseting the values of the cariables
+  //reseting the values of the variables
   highestPoint = 0;
   avgHeight = 0;
   rectCount = 0;
   
   for(let x = 0; x < width; x += rectWidth){
+
+    //randomizing using noise
     timeX += 0.01;
     rectHeight = map(noise(timeX), 0, 1, 0, height * 0.8);
 
+    //recording the number of rectangles
     rectCount++;
 
     //adding up all the rectangles to get the average
@@ -45,9 +47,8 @@ function drawRectangles(){
       xForHighestPoint = x;
     }
 
+    //drawing the terrain
     rect(x, height, rectWidth, rectHeight * -1);
-    
-
   }
 
   //drawing the flag
@@ -56,7 +57,7 @@ function drawRectangles(){
   //calculating the average
   avgHeight = avgHeight/rectCount;
     
-  //drawing the avrage line
+  //drawing the average line
   averageLine(height - avgHeight);
 }
 
@@ -73,7 +74,7 @@ function averageLine(avarage){
   rect(0,avarage ,width,10);
 }
 
-//draw main function
+//main draw function
 function draw() {
   background(225);
   rectTime += 0.03;

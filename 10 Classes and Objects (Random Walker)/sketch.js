09a211c103a1ac338ce6ec05e597833c661fa928
RadySPSD <6500688@spsd.sk.ca>
2024-04-09 15:19:56 -0600

    challenge
    challenge

--- 10 Classes and Objects (Random Walker)/sketch.js
+++ 10 Classes and Objects (Random Walker)/sketch.js
@@ -1,28 +1,42 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Classes and Objects
+// Mr. Scott
+// April 8th, 2024
+// A first look at making our own classes
 
+//let myWalker, myWalker2;  //objects can't be created before setup()
+let walkers = [];
+const NUM_WALKERS = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  myWalker
+  //set up some walker objects
+  for(let i = 0; i < NUM_WALKERS; i++){
+    let c = color(random(255),random(255),random(255));
+    walkers.push(new Walker(width/2,height/2,c));
+  }
+  noStroke();
+  background(0);
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  for(let w of walkers){
+    w.move();
+    w.display();
+  }
 }
 
 
-class walker{
+class Walker {
+
+  // Constructor
   constructor(x, y, c){
-    this.x = x; this.y = y; this.c = c;
+    this.x = x;   this.y = y;    this.c = c;
     this.speed = random(2,10);
     this.size = 5;
   }
 
+  // Class Methods
   display(){
     rectMode(CENTER);
     fill(this.c);
@@ -30,12 +44,19 @@ class walker{
   }
 
   move(){
-    //equally likely chance of 
-    let choice = Math.floor(random(4));
-    if (choice===0) this.x -=this.speed;
-    else if (choice === 1) this.X += this.speed;
-    else if (choice === 2) this.y -=
+    //equally likely chance of ↑ ↓ → ←
+    let choice = Math.floor(random(4)); //0, 1 ,2 ,3
+    if(choice===0) {
+      this.x -= this.speed;
+    } //LEFT
+    else if (choice === 1) {
+      this.x += this.speed;
+    } //RIGHT
+    else if (choice === 2) {
+      this.y -= this.speed;
+    } //UP
+    else if (choice === 3) {
+      this.y += this.speed;
+    } //DOWN
   }
-
 }
-

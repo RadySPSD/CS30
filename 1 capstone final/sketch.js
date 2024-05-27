859c87b4b6fc80fb04e438f10d71bd5044e526c5
RadySPSD <6500688@spsd.sk.ca>
2024-05-27 12:44:40 -0600

    day 8. working on menu button. having some issues. To do: finish the menu button

--- 1 capstone final/sketch.js
+++ 1 capstone final/sketch.js
@@ -9,14 +9,18 @@ let whichgame;
 //main setup function
 function setup() {
   createCanvas(900, 950);
-  square = new snakeGame();
   playSnake();
-  //whichgame = "snake"
+  //whichgame = "snake";
+
+  if (whichgame === "snake"){
+    square = new snakeGame();
+  }
   
 }
 
 // draw funcition that keeps on refreshing
 function draw() {
+  playSnake();
 
   if (whichgame === "snake"){
     square.display();
@@ -30,23 +34,28 @@ function draw() {
 function playSnake(){
     //snake
     fill(150);
-    rect(windowWidth/2,windowHeight/2,200,50);
-    textSize(50)
-    fill(0)
-    text("Snake",windowWidth/2 + 35,windowHeight/2,890)
-    if(mouseX >= windowWidth/2 && mouseX <= windowWidth/2 + 200 && mouseY >= windowHeight / 2 && mouseY <= windowHeight / 2 + 50 && mouseIsPressed === true){
-      whichgame = "snake"
+    rect(windowWidth/2 - 100,windowHeight/2 - 25,200,50);
+    textSize(50);
+    fill(0);
+    text("Snake",windowWidth/2 - 60,windowHeight/2 - 25,890);
+    if(mouseX >= windowWidth/2 - 100 && mouseX <= windowWidth/2 + 100 && 
+      mouseY >= windowHeight / 2 - 25 && mouseY <= windowHeight / 2 + 25 && 
+      mouseIsPressed === true){
+      whichgame = "snake";
+      print("snake");
     }
   }
 
 function keyPressed() {
   if (whichgame === "snake") {
-    square.controls(); 
+    square.controls();
   }
 }
 
 function mousePressed(){
-  square.restartButton();
-  square.MenuButton();    
+  if (whichgame === "snake"){
+    square.restartButton();
+    square.MenuButton();   
+  }
+ 
 }
-

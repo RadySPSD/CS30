6ffd083a2feb7f986dfc016397545b5d0033e24d
RadySPSD <160178043+RadySPSD@users.noreply.github.com>
2024-06-03 23:28:05 -0600

    day 14 b. In flappy bird you can now lose the game if you hit the pipes or if you fall down too much or up too much. I tried making the score text work, but for some reason the score is not registering. To do: Do some tweaks with the pipes, and fix the score issue and make a restart button.

--- 1 capstone final/flappyBirdGame.js
+++ 1 capstone final/flappyBirdGame.js
@@ -2,26 +2,51 @@ class flappyBird {
   constructor() {
     this.speed = 5;
     this.score = 0;
-
+    this.score = 0;
 
 
 
     this.bird = new Sprite(50, windowHeight / 2, 20, "d");
 
     this.obstacles = new Group();
+    
     this.obstacles.collider = 'k';
     this.obstacles.h = 300;
     this.obstacles.w = 50;
     this.obstacles.color = "green";
     world.gravity.y = 5;
 
+    setInterval(this.setScore,500);
+
     this.gapSize = this.gapSize;
     this.top = top;
     this.bottom = this.bottom;
+
+    this.bird.overlaps(this.obstacles,this.lose)
+
+    fill(0)
+    textSize(30)
+    text(this.score,50,50)
+  }
+
+  lose(){
+    //this.obstacles.color = "red";
+    noLoop();
+    
+    fill(0, 255, 0)
+    rect(windowWidth / 2 - 205, windowHeight / 2 - 65, 430, 100)
+    fill(255, 0, 0)
+    textSize(100)
+    text("game over", windowWidth / 2 - 200, windowHeight / 2)
+
+    this.score = 0;
+  }
+
+  setScore(){
+    this.score += 1;
   }
 
   display() {
-
 
     world.step();
     camera.x = this.bird.x + 100;
@@ -32,6 +57,8 @@ class flappyBird {
       this.bird.x = 100;
       this.bird.vel.x = 0;
       this.bird.vel.y = 0;
+      this.score = 0;
+      this.lose()
     }
 
     else if (this.bird.y < 50) {
@@ -39,11 +66,14 @@ class flappyBird {
       this.bird.x = 100;
       this.bird.vel.x = 0;
       this.bird.vel.y = 0;
+      this.score = 0;
+      this.lose()
     }
 
     //making obstacles
 
     if (frameCount % 45 === 0) {
+
       this.gapSize = random(80, 120);
 
       this.top = new this.obstacles.Sprite();
@@ -57,22 +87,11 @@ class flappyBird {
       this.bottom.vel.x = -3;
 
     }
-
-
   }
+
   controls() {
     if (kb.presses('space')) {
       this.bird.vel.y -= 3;
     }
-
-
   }
-
-  // spawn(){
-  //   this.gapSize = random(80,120)
-  //   this.top = new this.obstacles.Sprite;
-  //   this.top.y = random(-150,height/6)
-  //   this.top.x = this.bird.x + width
-  //   //print("obs")
-  // }
 }
